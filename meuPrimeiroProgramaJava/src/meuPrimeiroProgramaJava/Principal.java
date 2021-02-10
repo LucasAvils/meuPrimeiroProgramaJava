05435286199bcc593c378e2fe85f5768e63609a8
LucasAvils <63430004+LucasAvils@users.noreply.github.com>
2021-02-10 20:06:37 -0300

    Update Principal.java
    Alteração comentários

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -15,15 +15,16 @@ public class Principal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//SaudaÁ„o e pergunta o nome
-		System.out.println("Ol· Amigo \nQual È o seu nome?");
+		//Sauda√ß√£o e pergunta o nome
+		System.out.println("Ol√° Amigo \nQual √© o seu nome?");
 		
 		// ler uma string do console usando a biblioteca scanner
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		
-		//Exibir um string com saudaÁ„o especÌfica usando printf
-		System.out.printf("Ol· %s!", nome);
+		
+		//Exibir um string com sauda√ß√£o espec√≠fica usando printf
+		System.out.printf("Ol√° %s!", nome);
 		in.close();
 	}
 
