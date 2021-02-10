25d5f77708076492d3ddaaee97e5661565098896
LGame <LGame@LAPTOP-64VNP15P>
2021-02-10 19:59:18 -0300

    Alterações nos comentários

--- meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
+++ meuPrimeiroProgramaJava/src/meuPrimeiroProgramaJava/Principal.java
@@ -15,13 +15,14 @@ public class Principal {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		//Saudação e pergunta o nome
 		System.out.println("Olá Amigo \nQual é o seu nome?");
 		
 		// ler uma string do console usando a biblioteca scanner
 		Scanner in = new Scanner(System.in);
 		String nome = in.nextLine();
 		
-		//Exibir um string usando printf
+		//Exibir um string com saudação específica usando printf
 		System.out.printf("Olá %s!", nome);
 		in.close();
 	}
